76b3469061da14bd66787a0d8a5a074802e541e4
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2022-05-15 21:27:30 -0400

    add frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  state = {
+    clients: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('http://localhost:8080/users');
+    const body = await response.json();
+    this.setState({clients: body});
+  }
+
+  render() {
+    const {clients} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>Clients</h2>
+              {clients.map(client =>
+                  <div key={client.id}>
+                    {client.name} ({client.email})
+                  </div>
+              )}
+            </div>
+            <h1>Styling branch changed again.</h1>
+          </header>
+        </div>
+    );
+  }
+}
+export default App;
