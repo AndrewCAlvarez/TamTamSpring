e9e23b2316b57fbfa0be2e6059c06a2b8e39a6d5
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2022-05-23 06:07:52 -0400

    Moved files from older backend project into folder named 'old'.

--- Backend/src/main/java/com/Backend/Backend/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-// NOTE: This is old code that needs to be removed or reviewed.
-
-package com.Backend.Backend;
-
-public class UserNotFoundException extends RuntimeException {
-    UserNotFoundException(long id) {
-        super("Could not find user " + id);
-    }
-}
---
+++ Backend/src/main/java/com/Backend/Backend/old/UserRepository.java
@@ -0,0 +1,12 @@
+// // NOTE: This is old code that needs to be removed or reviewed.
+
+// package com.Backend.Backend.old;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+// // import org.springframework.data.repository.CrudRepository;
+
+// // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// // CRUD refers Create, Read, Update, Delete
+
+// public interface UserRepository extends JpaRepository<User, Long> {
+// }
